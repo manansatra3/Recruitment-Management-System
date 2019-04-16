24c54b6da4b0a69ad09e2b39151bbda2fd170b9e
Manan Satra <satramanan321@gmail.com>
2019-04-16 16:52:40 -0400

    UI changes and application form submission js

---
+++ public/js/applicationSubmissionForm.js
@@ -0,0 +1,25 @@
+function checkFormValidation() {
+    console.log ('here');
+    var firstName = document.getElementsByName('firstName')[0].nodeValue;
+    var lastName = document.getElementsByName('lastName')[0].nodeValue;
+    var email = document.getElementsByName('email')[0].nodeValue;
+    var phoneNumber = document.getElementsByName('phoneNumber')[0].nodeValue;
+    var street = document.getElementsByName('street')[0].nodeValue;
+    var city = document.getElementsByName('city')[0].nodeValue;
+    var zipCode = document.getElementsByName('zipCode')[0].nodeValue;
+    var state = document.getElementById('state').value;
+    var currentUniversity = document.getElementsByName('currentUniversity')[0].nodeValue;
+    var gpa = document.getElementById('gpa');
+    var degree = document.getElementsByName('degree')[0].nodeValue;
+    var major = document.getElementsByName('major')[0].nodeValue;
+    var graduation = document.getElementById('graduation');
+    console.log(graduation.value);
+    // if (firstName==null) {
+    //     alert('Enter First Name');
+    //     return;
+    // }
+    // if (lastName==null) {
+    //     alert('Enter Last Name');
+    //     return;
+    // }
+}
