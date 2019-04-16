5af3092b5600d202e671fc51f0d0b7f6f4427479
Manan Satra <satramanan321@gmail.com>
2019-04-16 18:12:45 -0400

    job posting route middleware

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const jobSeekerRoutes = require ('./jobSeekerRoutes');
 const loginRoutes = require("./login");
 const signupRoutes = require("./signup");
+const jobPosting = require('./jobPosting');
 
 const constructorMethod = app => {
     app.use("/applicant", jobSeekerRoutes);
+    app.use("/employer", jobPosting);
     app.use("/signup", signupRoutes);
     app.use("/", loginRoutes);
     app.use("*", (req, res) => {
