80b2e1dd5b55e6992a95058dd53b1898b4c799e2
Manan Satra <satramanan321@gmail.com>
2019-04-16 12:00:49 -0400

    changes 2 form & initialization of route to submit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const jobSeekerRoutes = require ('./jobSeekerRoutes');
+const loginRoutes = require("./login");
+
+const constructorMethod = app => {
+    app.use("/applicant", jobSeekerRoutes);
+    app.use("/", loginRoutes);
+    app.use("*", (req, res) => {
+        return res.redirect("/");
+    });
+  };
+  
+  module.exports = constructorMethod;
