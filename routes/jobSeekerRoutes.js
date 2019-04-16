1217f7bfc65554c446f5e2438e910e57b18a8ee4
Manan Satra <satramanan321@gmail.com>
2019-04-16 18:13:05 -0400

    profile submission for new user

--- routes/jobSeekerRoutes.js
+++ routes/jobSeekerRoutes.js
@@ -5,4 +5,9 @@ const data = require("../data");
 router.get('/submitApplication', (req, res)=> {
     res.render('applicationPages/submitApplication.handlebars')
 });
+
+router.get("/profile", (req, res)=>{
+    res.render('profileSubmission.handlebars');
+});
+
 module.exports = router;
