eae5af9686a055603dc40ae0954fbd204fe69d90
Christina Li <cli50@stevens.edu>
2019-04-19 13:35:09 -0400

    wrote async functions to store user login information

---
+++ data/signup.js
@@ -0,0 +1,56 @@
+const mongoCollections = require("./collection");
+const signup = mongoCollections.signup;
+
+module.exports ={
+    /**
+     * stores the signup information into a mongoDB
+     * @param {*} type employer or applicant
+     * @param {*} username username they set 
+     * @param {*} email validated email
+     * @param {*} password password they set
+     */
+    async signup(type, username, email, password){
+        //errors
+        const person = await signup();
+        let newPerson = {
+            type,
+            username,
+            email,
+            password
+        };
+        const insert = await person.insertOne(newPerson);
+        if(insert.insertedCount === 0){
+            throw "Could not add person";
+        }
+        const newId = insert.insertedId;
+        return await this.get(newId);
+    },
+
+    /**
+     *
+     * @returns an array of all the people who signed up in the collection
+     */
+    async allPeople(){
+        const people = await signup();
+        return await people.find({}).toArray();
+    },
+
+    /**
+     * get the person who signed up from the database
+     * @param {string} id
+     * @returns the person from the database
+     */
+    async get(id){
+        //given id, return the animal from the database
+        if(!id){
+            throw "Error: no id was provided";
+        }
+        const person = await signup();
+        const findPerson = await person.findOne({_id: id});
+        if(findPerson === null){
+            throw "No person with that id";
+        }
+        return findPerson;
+    }
+
+}
