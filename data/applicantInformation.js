4963a936d4eca51041068c92426fe547c1d0ab18
Christina Li <cli50@stevens.edu>
2019-04-24 20:05:14 -0400

    applicant can now update their information

--- data/applicantInformation.js
+++ data/applicantInformation.js
@@ -8,62 +8,64 @@ module.exports = {
      *update the applicant information when they sign up
      *
      */
-    async updateApplicant(name, email, phoneNumber, education, work, documents, comments){
+    async applicantInformation(id, name, email, phoneNumber, education, work){
         const info = await information();
         //objId should be coming from the signup database where that Id is stored
-        const objId = ObjectId(email);
+        const objId = ObjectId(id);
 
         let updatedInfo = {
             id: objId,
-            name : {
-                firstName: name.split(" ")[0],
-                lastName: name.split(" ")[1].trim()
-            },
+            name,
             email,
             phoneNumber,
-            education : {
-                currentEducation: {
-                    universityName,
-                    gpa,
-                    degreee,
-                    major,
-                    graduationYear
-                },
-                previousEducation: {
-                    universityName,
-                    gpa,
-                    degreee,
-                    major,
-                    graduationYear
-                }
-            },
-            work: {
-                currentWork: {
-                    employerName,
-                    employerCity,
-                    currentPosition
-                },
-                previousWork: {
-                    employerName,
-                    employerCity,
-                    currentPosition
-                }
-            },
-            documents : {
-                resume,
-                transcript,
-                coverLetter,
-                extraUpload
-            },
-            comments
+            education,
+            work,
         }
         const insert = await info.insertOne(updatedInfo);
         
         if(insert.insertedCount === 0){
             throw "Could not add title";
         }
-        
+
         return await this.get(insert.insertedId);
+    },
+    async getApplicantById(id){
+        if(!id){
+            throw "Error: no id was provided";
+        }
+        const info = await information();
+        const applicant = info.findOne({_id: id});
+        if(!applicant){
+            throw `Error: no Applicant was found with this id: ${id}`
+        }
+        return applicant;
+    },
+    async updateApplicantInfo(id, updatedInfo){
+        const info = await information();
+        const updated = {};
+        if(updatedInfo.name){
+            updated.name = updatedInfo.name;
+        }
+        if(updatedInfo.email){
+            updated.email = updatedInfo.email;
+        }
+        if(updatedInfo.phoneNumber){
+            updated.phoneNumber = updatedInfo.phoneNumber;
+        }
+        if(updatedInfo.education){
+            updated.education = updatedInfo.education;
+        }
+        if(updatedInfo.work){
+            updated.work = updatedInfo.work;
+        }
+        let updatedCommand = {
+            $set: updated
+        };
+        const query = {
+            _id: id
+        };
+        await info.updateOne(query, updatedCommand);
+        return await this.getApplicantById(id);
     }
 
 }
