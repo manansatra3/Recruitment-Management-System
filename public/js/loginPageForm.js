0d708bd27e6f5c164024d9b6ef74e7c0ed9b5b9a
Mohitdb <36172336+Mohitdb@users.noreply.github.com>
2019-05-01 18:47:47 -0400

    setting a base for after login

--- public/js/loginPageForm.js
+++ public/js/loginPageForm.js
@@ -1,49 +1,19 @@
 // //Create a reference to the form
-// const myForm = document.getElementById("static-form1");
+const myForm = document.getElementById("static-form1");
 
-// if (myForm) {
+ if (myForm) {
 //     //Get a reference to the text_input field
+    const textInput = document.getElementById("username");
+    const passwordInput = document.getElementById("password");
+    
 //     const textInput = document.getElementById("text1");
 
 //     //Add an event listener for the form submit
-//     myForm.addEventListener("submit", event => {
+     myForm.addEventListener("submit", event => {
 //         //We need to prevent the default behavior of the form submit
-//         event.preventDefault();
+         event.preventDefault();
+         myForm.submit()
 
-//         /*  Now we check to make sure the user entered a value in the input box
-//             We are only concerned if they entered something, we do not need to
-//             validate if it's a number, string etc.. in this example
-//         */
-//         if (textInput.value) {
-//             //we hide the error div in case it's visible
-//             $("#usererror").hide();
-//             if(textInput.value)
-            
 
-//             //We create the list item element variable
-//             const li = `<li> ${textInput.value} </li>`
-
-//             //we add the li element created above to the UL
-//             $("#myList").append(li);
-
-//             //We then reset the form
-//             $("#myForm").trigger('reset');
-//             /*  you can also do this by just clearing the text input as shown below
-//                 if you want to reset all fields, use the reset example above
-//                 if you want to just clear certain form fields you can use the example below
-//                 $("#text_input").val('');
-//             */
-
-//             //then set the cursor focus to the input box
-//             $('#text_input').focus();
-//         } else {
-//             //If the user did not enter input, we show the error div and text
-//             $("#error").show();
-//             $("#error").html("You Need to supply an input value!");
-
-//             //then set the cursor focus to the input field
-//             $('#text_input').focus();
-//         }
-
-//     });
-// }
+     });
+ }
