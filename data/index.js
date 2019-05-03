9d5b415c9b6b6d6ea197e83736d0c7d1d206059b
Christina Li <cli50@stevens.edu>
2019-05-02 22:59:21 -0400

    will finish database tomorrow

--- data/index.js
+++ data/index.js
@@ -1,10 +1,16 @@
 const usersData = require("./users");
 const applicantData = require("./applicantInformation");
 const submitApplication = require("./submitApplication");
-const getApplicantDocuments = require('./getApplicantDocuments')
+const getApplicantDocuments = require('./getApplicantDocuments');
+const jobPost = require('./jobPost');
+const employerData = require('./employerInformation');
+
+
 module.exports = {
   usersData,
   applicantData,
+  employerData,
+  jobPost,
   submitApplication,
   getApplicantDocuments
 };
---
+++ data/jobPost.js
@@ -0,0 +1,80 @@
+const mongoCollections = require("./collection");
+const jobData = mongoCollections.jobPost;
+
+module.exports = {
+    /**
+     *update the applicant applicantInfo when they sign up
+     *
+     */
+    async applicantapplicantInfo(id, name, email, phoneNumber, education, work){
+        const info = await aapplicantInfo();
+        //objId should be coming from the signup database where that Id is stored
+        const objId = ObjectId(id);
+        let updatedInfo = {
+            id: objId,
+            name,
+            email,
+            phoneNumber,
+            education,
+            work,
+        }
+        const insert = await info.insertOne(updatedInfo);
+        
+        if(insert.insertedCount === 0){
+            throw "Could not add title";
+        }
+
+        return await this.get(insert.insertedId);
+    },
+    async getApplicantById(id){
+        if(!id){
+            throw "Error: no id was provided";
+        }
+        const info = await applicantInfo();
+        const applicant = info.findOne({_id: id});
+        if(!applicant){
+            throw `Error: no Applicant was found with this id: ${id}`
+        }
+        return applicant;
+    },
+    async updateApplicantInfo(id, updatedInfo){
+        const info = await applicantInfo();
+        const updated = {};
+        if(updatedInfo.name){
+            updated.name = updatedInfo.name;
+        }
+        if(updatedInfo.email){
+            updated.email = updatedInfo.email;
+        }
+        if(updatedInfo.phoneNumber){
+            updated.phoneNumber = updatedInfo.phoneNumber;
+        }
+        if(updatedInfo.education){
+            updated.education = updatedInfo.education;
+        }
+        if(updatedInfo.work){
+            updated.work = updatedInfo.work;
+        }
+        let updatedCommand = {
+            $set: updated
+        };
+        const query = {
+            _id: id
+        };
+        await info.updateOne(query, updatedCommand);
+        return await this.getApplicantById(id);
+    },
+    async getAllUsers(){
+        const info = await applicantInfo();
+        return info.find({}).toArray();
+    },
+
+    async removeApplicant(id){
+        if(!id){
+            throw "Error: no id was provided";
+        }
+        const info = await applicantInfo();
+        return await info.deleteOne({_id: id});
+    }
+
+}
