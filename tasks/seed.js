c34a2470c68c162168d9b996ef49b7cab6b60f33
Mohitdb <36172336+Mohitdb@users.noreply.github.com>
2019-05-09 12:50:24 -0400

    seeded one recruiter

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,14 +1,15 @@
-const dbConnection = require("../data/mongoConnection");
+const dbConnection = require("../data/connection");
 const data = require("../data/");
 const users = data.usersData;
 
+//before executing please run the command "node tasks/seed.js once"
 async function main() {
   const db = await dbConnection();
-  await db.dropDatabase();
+  //await db.dropDatabase();
 
   const user = await users.signup("Recruiter", "cli40","cwe@gmail.com", "Ball1714!");
   console.log("Done seeding database");
-  await db.close();
+  //await db.close();
 }
 
 main();
