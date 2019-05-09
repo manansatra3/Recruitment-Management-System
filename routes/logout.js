eee4b92ee91572d06d6a54d96ac0cbb4559aba55
Mohitdb <36172336+Mohitdb@users.noreply.github.com>
2019-05-09 15:09:16 -0400

    session destroyed

--- routes/logout.js
+++ routes/logout.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 
 router.get('/', async (req, res) => {
+    req.session.destroy((err) => {
+        if(err) {
+            return console.log(err);
+        }
+        res.render("logout");
+    });
     res.render('logout')
 });
 
