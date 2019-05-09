ed6b44be38bc99fcd258ce8af00eeee3cefce139
Katya <katya@Katyas-MacBook-Pro.local>
2019-05-09 16:32:07 -0400

    updated seed file

--- tasks/seed.js
+++ tasks/seed.js
@@ -9,23 +9,23 @@ async function main() {
   //await db.dropDatabase();
 
   //recruiters
-  const recruiter1 = await users.signup("Recruiter", "christina123","christina@gmail.com", "Ball1714!");
-  const recruiter2 = await users.signup("Recruiter", "manan123","manan@gmail.com ", "manan@123");
-  const recruiter3 = await users.signup("Recruiter", "mohit123","mohit@gmail.com", "mohit@123");
-  const recruiter4 = await users.signup("Recruiter", "peng123","peng@gmail.com", "peng@123");
-  const recruiter5 = await users.signup("Recruiter", "katya123","katya@gmail.com", "katya@123");
+  const recruiter1 = await users.signup("Recruiter", "christina123","christina@gmail.com", "Ball1714!", "Christina", "Lee", "012-345-6789");
+  const recruiter2 = await users.signup("Recruiter", "manan123","manan@gmail.com ", "manan@123", "Manan", "Satra", "012-345-6789" );
+  const recruiter3 = await users.signup("Recruiter", "mohit123","mohit@gmail.com", "mohit@123", "Mohit", "Bhagchandani", "012-345-6789");
+  const recruiter4 = await users.signup("Recruiter", "peng123","peng@gmail.com", "peng@123", "Peng", "Zang", "012-345-6789");
+  const recruiter5 = await users.signup("Recruiter", "katya123","katya@gmail.com", "katya@123", "Katya", "Bevinova", "012-345-6789");
 
   //applicants
-  const applicant1 = await users.signup("Applicant", "john123","john@gmail.com", "john@123");
-  const applicant2 = await users.signup("Applicant", "cynthia123","cynthia@gmail.com", "cynthia@123");
-  const applicant3 = await users.signup("Applicant", "sam123","sam@gmail.com", "sam@123");
-  const applicant4 = await users.signup("Applicant", "mike123","mike@gmail.com", "mike@123");
-  const applicant5 = await users.signup("Applicant", "melani123","melani@gmail.com", "melani@123");
-  const applicant6 = await users.signup("Applicant", "christopher123","christopher@gmail.com", "christopher@123");
-  const applicant7 = await users.signup("Applicant", "david123","david@gmail.com", "david@123");
-  const applicant8 = await users.signup("Applicant", "henry123","henry@gmail.com", "henry@123");
-  const applicant9 = await users.signup("Applicant", "bob123","bob@gmail.com", "bob@123");
-  const applicant10 = await users.signup("Applicant", "kyle123","kyle@gmail.com", "kyle@123");
+  const applicant1 = await users.signup("Applicant", "john123","john@gmail.com", "john@123", "John", "Doe", "012-345-6789");
+  const applicant2 = await users.signup("Applicant", "cynthia123","cynthia@gmail.com", "cynthia@123", "Cynthia", "Dom", "012-345-6789");
+  const applicant3 = await users.signup("Applicant", "sam123","sam@gmail.com", "sam@123", "Sam", "Smith", "012-345-6789");
+  const applicant4 = await users.signup("Applicant", "mike123","mike@gmail.com", "mike@123", "Mike", "Johnson", "012-345-6789");
+  const applicant5 = await users.signup("Applicant", "melani123","melani@gmail.com", "melani@123", "Melani", "Krumpin", "012-345-6789");
+  const applicant6 = await users.signup("Applicant", "christopher123","christopher@gmail.com", "christopher@123", "Christopher", "Robin", "012-345-6789");
+  const applicant7 = await users.signup("Applicant", "david123","david@gmail.com", "david@123", "David", "Copperfield", "012-345-6789");
+  const applicant8 = await users.signup("Applicant", "henry123","henry@gmail.com", "henry@123", "Henry", "Pork", "012-345-6789");
+  const applicant9 = await users.signup("Applicant", "bob123","bob@gmail.com", "bob@123", "Bob", "Trinket", "012-345-6789");
+  const applicant10 = await users.signup("Applicant", "kyle123","kyle@gmail.com", "kyle@123", "Kyle", "Smith", "012-345-6789");
 
 
 /*
